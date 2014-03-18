f98c416e05cdeeceaa43cb81c273175bcf22c7bb
rpessoa <rpessoa@ubuntu.ubuntu-domain>
2014-03-18 14:59:15 +0000

    Add Structure of Framework

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+include 'lib/bones.php';
+
+get('/', function($app) {
+	$app->set('message', 'Welcome Back!');
+	$app->render('home');
+});
+
+get('/signup', function($app) {
+	$app->render('signup');
+});
+
+get('/say/:message', function($app) {
+	$app->set('message', $app->request('message'));
+	$app->render('home');
+});
+
+post('/signup', function($app) {
+    $user = new stdClass;
+    $user->name = $app->form('name');
+    $user->email = $app->form('email');
+    echo json_encode($user);
+
+    $curl = curl_init();
+    // curl options
+    $options = array(
+    CURLOPT_URL => 'localhost:5984/verge',
+    CURLOPT_POSTFIELDS => json_encode($user),
+    CURLOPT_HTTPHEADER => array ("Content-Type: application/json"),
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "utf-8",
+    CURLOPT_HEADER => false,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_AUTOREFERER => true
+    );
+    
+    curl_setopt_array($curl, $options);
+    curl_exec($curl);
+    curl_close($curl);
+    $app->set('message', 'Thanks for Signing Up ' . $app->
+    form('name') . '!');
+    $app->render('home');
+
+});
---
+++ views/signup.php
@@ -0,0 +1,9 @@
+Signup
+
+<form action="<?php echo $this->make_route('/signup') ?>" method="post">
+	<label for="name">Name</label>
+	<input id="name" name="name" type="text"> <br />
+	<label for="email">Email</label>
+        <input id="email" name="email" type="text"> <br />
+        <input type="Submit" value="Submit">
+</form>
