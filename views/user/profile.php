c8d119d6d31b3dcb5c76d437009875a4c5ded114
rpessoa <rpessoa@ubuntu.ubuntu-domain>
2014-03-21 15:19:50 +0000

    Create the get_by_username function to show the user profile

---
+++ views/user/profile.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+    <h1>User Profile</h1>
+    <div class="container">
+        <div class="row">
+            <?php var_dump($user); ?>
+        </div>
+    </div>
+</div>
+
