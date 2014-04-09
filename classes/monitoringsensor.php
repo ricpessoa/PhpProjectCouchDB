e98bc9ce767c47878c6ad09e32c41277408a892d
linuxrp <linuxrp@hotmail.com>
2014-04-09 15:45:51 +0100

    finished the partial _temperature view to show the values by time in chart for each device/sensor

---
+++ classes/monitoringsensor.php
@@ -0,0 +1,58 @@
+<?php
+
+class MonitoringSensor extends Base {
+
+    protected $subtype;
+    protected $value;
+    protected $timestamp;
+    protected $mac_address;
+    protected $arrayValues;
+    protected $arrayTimes;
+
+    public function __construct($type) {
+        parent::__construct($type);
+    }
+
+    public function getMonitoringSensorByKeys($username, $macAddress, $subtype) {
+        $bones = new Bones();
+        $bones->couch->setDatabase($username);
+        $sensorsTime = array();
+        $sensorsValues = array();
+        try {//\[
+            //foreach ($bones->couch->get('_design/application/_view/getMonitoringSensor?key=\["' . $macAddress . '","' . $subtype . '"\]')->body->rows as $_monitoring) {
+            $monitoringSensor = new MonitoringSensor();
+
+            foreach ($bones->couch->get('_design/application/_view/getMonitoringSensor?key=["' . $macAddress . '","' . $subtype . '"]&limit=24&descending=false')->body->rows as $_monitoring) {
+                //$monitoringSensor = new MonitoringSensor();
+                //$monitoringSensor->_id = $_monitoring->id;
+                //$monitoringSensor->_rev = $_monitoring->value->_rev;
+                //$monitoringSensor->type = $_monitoring->value->type;
+                //$monitoringSensor->subtype = $_monitoring->value->date_created;
+                //$monitoringSensor->value = $_monitoringgetArrayValues();
+                //$monitoringSensor->timestamp = $_monitoring->value->timestamp;
+                //$monitoringSensor->mac_address = $_monitoring->value->mac_address;
+                //$monitoringSensor
+                array_push($sensorsTime, date('d/m H:i:s' , $_monitoring->value->timestamp));
+                array_push($sensorsValues, $_monitoring->value->value);
+
+                //array_push($sensorsMonitoring, $monitoringSensor->value);
+            }
+            $monitoringSensor->arrayTimes = $sensorsTime;
+            $monitoringSensor->arrayValues = $sensorsValues;
+            if(sizeof(arrayValues)>0)
+                return $monitoringSensor;
+        } catch (Exception $exc) {
+            return NULL;
+        }
+        return NULL;
+    }
+
+    public function getArrayValues() {
+        return json_encode($this->arrayValues);
+    }
+
+    public function getArrayTimes() {
+        return json_encode($this->arrayTimes);
+    }
+
+}
--- views/sensors/_temperature.php
+++ views/sensors/_temperature.php
@@ -1,49 +1,62 @@
-<script>
-    $(function() {
-        $('#container<?php echo $i; ?>').highcharts({
-            chart: {
-                type: 'spline'
-            },
-            title: {
-                text: 'Average Temperature'
-            },
-            xAxis: {
-                categories: ['10:00', '10:10', '10:20', '10:30', '10:40', '10:50',
-                    '11:00', '11:10', '11:20', '11:30', '11:40', '11:50', '12:00']
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature'
-                },
-                labels: {
-                    formatter: function() {
-                        return this.value + '°'
-                    }
-                }
-            },
-            tooltip: {
-                crosshairs: true,
-                shared: true
-            },
-            plotOptions: {
-                spline: {
-                    marker: {
-                        radius: 4,
-                        lineColor: '#666666',
-                        lineWidth: 1
-                    }
-                }
-            },
-            series: [{
-                    name: 'Temperature',
-                    marker: {
-                        symbol: 'diamond'
+<?php
+$monitoringsensors = MonitoringSensor::getMonitoringSensorByKeys(User::current_user(), $device->_id, "temperature");
+if ($monitoringsensors != NULL || sizeof($monitoringsensors) > 0) {
+    //echo '<br>' . $monitoringsensors->getArrayTimes() . '<br>' . $monitoringsensors->getArrayValues();
+    if (sizeof($monitoringsensors->arrayValues) > 0) {
+        ?>
+        <div id = "container<?php echo $i; ?>" style = "height: auto;width: 600px; margin:0 auto;"></div>
+
+        <script>
+            $(function() {
+                $('#container<?php echo $i; ?>').highcharts({
+                    chart: {
+                        type: 'spline'
                     },
-                    data: [4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-                }]
-        });
-    });
-</script>
-
-
-<div id="container<?php echo $i; ?>" style="height: auto;width: 600px; margin:0 auto;"></div>
+                    title: {
+                        text: 'Average Temperature'
+                    },
+                    xAxis: {
+                        categories: <?php echo $monitoringsensors->getArrayTimes(); ?>
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Temperature'
+                        },
+                        labels: {
+                            formatter: function() {
+                                return this.value + '°'
+                            }
+                        }
+                    },
+                    tooltip: {
+                        crosshairs: true,
+                        shared: true
+                    },
+                    plotOptions: {
+                        spline: {
+                            marker: {
+                                radius: 4,
+                                lineColor: '#666666',
+                                lineWidth: 1
+                            }
+                        }
+                    },
+                    series: [{
+                            name: 'Temperature',
+                            marker: {
+                                symbol: 'diamond'
+                            },
+                            data: <?php echo $monitoringsensors->getArrayValues(); ?>
+                        }]
+                });
+            });
+        </script>
+    <?php } else {
+        ?>
+        <div class="alert alert-info">
+            Not yet received any information from Temperature sensor!
+        </div>
+        <?php
+    }
+}
+?>
