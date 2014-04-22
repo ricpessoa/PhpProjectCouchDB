d8fd6b7224d7c76786acaa15e0643e9b2190b51b
linuxrp <linuxrp@hotmail.com>
2014-04-22 18:06:27 +0100

    Pass the edit device to partial views (safezones, tempertature)

---
+++ views/devices/_safezone.php
@@ -0,0 +1,94 @@
+ <?php if ($numberSafezones != 0) { ?>
+        <!-- Open Layer 
+        <script src="http://openlayers.org/api/OpenLayers.js"></script>
+        <script src="http://mapstraction.com/mxn/build/latest/mxn.js?(openlayers)" type="text/javascript"></script>
+        -->
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDqUwWysKF_x_SkHDgB-q_NeaqBHpPTlME&sensor=false"></script>
+        <script src="http://mapstraction.com/mxn/build/latest/mxn.js?(googlev3)" type="text/javascript"></script>
+        <script type="text/javascript" src="<?php echo $this->make_route('/js/mapfunctions.js') ?>"></script>
+
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span4">
+                    <!--Sidebar left content to TABLE SAFEZONE-->
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Option</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            $str_safezones = "";
+
+                            foreach (json_decode($jsonSafezones) as $safezone):
+                                ?>
+                                <tr>
+                                    <td><?php echo $i; ?></td>
+                                    <td><?php echo $safezone->name; ?></td>
+                                    <td>
+                                        <button id="edit_safezone" class="btn btn-info btn-small">Edit</button>
+                                        <button data-toggle="modal" data-id="<?php echo $safezone->_id; ?>" data-rev="<?php echo $safezone->_rev; ?>" title="Delete this Safezone" class="open-deleteSafezoneModal btn btn-danger  btn-small" href="#deleteSafezoneModal">Delete</button>
+                                    </td>
+                                </tr>
+                                <?php
+                                $i = $i + 1;
+                            endforeach;
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="span8">
+                    <!--Body content to MAP-->
+                    <div id="map"></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- MODAL DELETE SAFEZONE -->
+        <div class="modal fade" id="deleteSafezoneModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display:none">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Delete Safezone</h4>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to permanently delete the Safezone?
+                    </div>
+                    <div class="modal-footer">
+                        <form id="form_delete_safezone" method="post">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                //var map = new mxn.Mapstraction('map', 'openlayers');
+                window.map = new mxn.Mapstraction('map', 'googlev3');
+                //var latlon = new mxn.LatLonPoint(51.50733, -0.12769);
+                //map.addExtras();
+                map.enableScrollWheelZoom();
+                //window.geocoder = new google.maps.Geocoder();
+                //map.setCenterAndZoom(latlon, 10);
+                //console.log('<?php echo "[" . substr($str_safezones, 0, -1) . "]"; ?>');
+                getSafezones('<?php echo '{"safezones"' . ":" . $jsonSafezones . "}"; ?>');
+            });
+
+            $(document).on("click", ".open-deleteSafezoneModal", function() {
+                var myDocId = $(this).data('id');
+                var myDocRev = $(this).data('rev');
+                var finalURL = '/PhpProjectCouchDB/deletesafezone/' + myDocId + '/' + myDocRev;
+                $(".modal-footer #form_delete_safezone").attr('action', finalURL);
+
+            });
+        </script>
+
+    <?php } ?>
---
+++ views/devices/_temperature.php
@@ -0,0 +1,21 @@
+<div class="control-group">
+    <div id="div_propreties_temperature"> 
+        <div class="well">
+            <h4>Temperature Settings</h4>
+            <div class="control-group">
+                <label class="control-label">Minimum Temperature Notification</label>
+                <div class="controls">
+                    <input id="min_temp_notification" type="number" min="22" max="28" name="min_temp_notification" />
+                    <p class="help-block">NEED VALIDATE THE VALUE TO ABNORMAL LIKE hypothermia VALUES</p>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label">Maximum Temperature Notification</label>
+                <div class="controls">
+                    <input id="max_temp_notification" type="number" min="22" max="28" name="max_temp_notification" />
+                    <p class="help-block">NEED VALIDATE THE VALUE TO ABNORMAL LIKE FEVER VALUES</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
